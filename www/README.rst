e75ea195109b9089243109964f7031faa30f2a00
Jeremie Pardou-Piquemal <jrmi@jeremiez.net>
2017-09-08 10:39:57 +0200

    Add webclient

---
+++ www/README.rst
@@ -0,0 +1,16 @@
+Install
+=======
+
+With nvm: ::
+
+    nvm install
+    nvm use
+    npm upgrade -g npm
+    npm init
+    #npm install -g npx
+
+    npm install
+
+Build: ::
+    npx webpack
+
---
+++ www/src/index.js
@@ -0,0 +1,12 @@
+import _ from 'lodash';
+
+function component() {
+    var element = document.createElement('div');
+  
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
+  
