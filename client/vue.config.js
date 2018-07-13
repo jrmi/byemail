430a4803cc19c6a30c9dc2f9bcf948f1236533ce
Jeremie Pardou-Piquemal <jrmi@jeremiez.net>
2018-07-13 18:35:23 +0200

    Fix #57 proxy reconfiguration

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   devServer: {
-    proxy: 'http://localhost:8000'
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        ws: true,
+        changeOrigin: false
+      },
+      '/login': {
+        target: 'http://localhost:8000'
+      },
+      '/logout': {
+        target: 'http://localhost:8000'
+      }
+    }
   }
 }
