741071beec1c089904b40eb7d4ae5ae447013e7e
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2019-02-03 18:42:10 +0100

    Send mail script simplification

--- byemail/scripts/send_test_mail.py
+++ byemail/scripts/send_test_mail.py
@@ -4,23 +4,9 @@ import sys
 import email
 import codecs
 
-import chardet
-
 from email import policy
 from email.parser import BytesParser
 
-
-def decode(s, encodings=('utf8', 'iso-8859-1', 'iso-8859-2', 'latin1')):
-    detected = chardet.detect(s)
-    encodings = [detected['encoding']] + list(encodings)
-    for encoding in encodings:
-        try:
-            success = s.decode(encoding)
-            print('Decoded with %s' % encoding)
-            return success
-        except UnicodeDecodeError:
-            pass
-    return s.decode('ascii', 'ignore')
 
 def mail_from_file(mail_path):
 
@@ -28,8 +14,7 @@ def mail_from_file(mail_path):
         #import ipdb; ipdb.set_trace()
         ff = in_file.read()
 
-    decoded = decode(ff)
-    msg = BytesParser(policy=policy.default).parsebytes(decoded.encode('utf-8'))
+    msg = BytesParser(policy=policy.default).parsebytes(ff)
 
     return msg
 
@@ -39,18 +24,10 @@ if __name__ == "__main__":
 
     client = Client('localhost', 8025)
 
-    for arg in sys.argv[1:]:
+    for arg in sys.argv:
         print(f"Sending {arg}")
-        msg = mail_from_file(arg)
-        #import ipdb; ipdb.set_trace()
-
         try:
-           msg.as_string().encode('utf-8')
+            msg = mail_from_file(arg)
+            r = client.sendmail('from@localhost', ['to@localhost'], msg.as_bytes())
         except:
-            print('File %r failed to convert' % arg)
-            #import ipdb; ipdb.set_trace()
-        else:
-            r = client.sendmail('from@localhost', ['to@localhost'], msg.as_string().encode('utf-8'))
-
-
-
+            print(f"EEEEEE - Fail to send {arg}")
