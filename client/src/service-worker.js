9ef563038e1ccd392de20ce5cff9acc2e623eedb
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2019-02-18 18:25:23 +0100

    Fix #7 click on notification show browser

--- client/src/service-worker.js
+++ client/src/service-worker.js
@@ -12,12 +12,44 @@ self.addEventListener('push', function (event) {
   // on https://developer.mozilla.org/en-US/docs/Web/API/PushMessageData.
   const payload = event.data ? event.data.text() : 'You have a new message'
 
-  // Keep the service worker alive until the notification is created.
   event.waitUntil(
-    // Show a notification with title 'ServiceWorker Cookbook' and use the payload
-    // as the body.
-    self.registration.showNotification('Byemail', {
-      body: payload
+    // Retrieve a list of the clients of this service worker.
+    self.clients.matchAll().then(function (clientList) {
+      // Check if there's at least one focused client.
+      var focused = clientList.some(function (client) {
+        return client.focused
+      })
+
+      if (focused) {
+        // Focused
+      } else if (clientList.length > 0) {
+        // Click to focus
+      } else {
+        // Reopen
+      }
+
+      return self.registration.showNotification('Byemail', {
+        body: payload,
+        tag: 'newmail'
+      })
+    })
+  )
+})
+
+// Register event listener for the 'notificationclick' event.
+self.addEventListener('notificationclick', function (event) {
+  console.log('notif click')
+  event.waitUntil(
+    // Retrieve a list of the clients of this service worker.
+    self.clients.matchAll().then(function (clientList) {
+      console.log('clients', clientList)
+      // If there is at least one client, focus it.
+      if (clientList.length > 0) {
+        return clientList[0].focus()
+      }
+
+      // Otherwise, open a new page.
+      return self.clients.openWindow('/index.html')
     })
   )
 })
